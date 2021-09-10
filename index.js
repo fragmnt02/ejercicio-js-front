7cd5eb9c730f3053b22f578c4a18a4e1622e1c41
MarcoAntonioAS17 <antonioarcosmarco@gmail.com>
2021-09-09 20:54:00 -0500

    Folder y ventana emergente, falta css para mejorar

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 window.onload = function () {
     const objeto = document.getElementById('objeto');
+    const folder = document.getElementById('folder');
+
     let presionado = false;
     let difX, difY;
+    let contador = 0;
+
     objeto.addEventListener('mousedown', (evento) => {
         const objeto = document.getElementById('objeto');
         const objRectangulo = objeto.getBoundingClientRect();
@@ -18,6 +22,29 @@ window.onload = function () {
         document.removeEventListener('mousemove', mover);
     });
 
+    folder.addEventListener('click', () => {
+        contador++;
+        if (contador === 2){
+            console.log("Abriendo folder");
+            contador=0;
+            abrirFolder();
+        }
+    }
+    );
+
+    folder.addEventListener('mousedown',(evento) => {
+        const objRectangulo = folder.getBoundingClientRect();
+        const objX = objRectangulo.left;
+        const objY = objRectangulo.top;
+        difX = evento.clientX - objX;
+        difY = evento.clientY - objY;
+        document.addEventListener('mousemove', moverFolder);
+    });
+
+    folder.addEventListener('mouseup', () => {
+        document.removeEventListener('mousemove', moverFolder);
+    });
+
     /*
     objeto.addEventListener('click', () => {
         presionado = !presionado;
@@ -32,9 +59,27 @@ window.onload = function () {
     function mover(evento) {
         const x = evento.clientX;
         const y = evento.clientY;
-        const objeto = document.getElementById('objeto');
         objeto.style.left = `${x - difX}px`;
         objeto.style.top = `${y - difY}px`;
+    }
+
+    function moverFolder(evento) {
+        const x = evento.clientX;
+        const y = evento.clientY;
+        folder.style.left = `${x - difX}px`;
+        folder.style.top = `${y - difY}px`;
+    }
+
+    function abrirFolder(){
+        const venta = document.createElement('div');
+        venta.style.width='1000px';
+        venta.style.height='600px';
+        venta.style.position='absolute';
+        venta.style.left = `${window.innerWidth/2 - 500}px`;
+        console.log(window.innerHeight);
+        venta.style.top = `${window.innerHeight/2 - 300}px`;
+        venta.style.backgroundColor='red';
+        document.body.appendChild(venta);
     }
 }
 
